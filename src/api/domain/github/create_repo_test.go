5cdda406ba688c0b3b0ad91f9ca3c81ae015bd94
novdov <rpwjd56@gmail.com>
2020-04-22 23:54:31 +0900

    Add domain

---
+++ src/api/domain/github/create_repo_test.go
@@ -0,0 +1,29 @@
+package github
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateRepoRequestAsJson(t *testing.T) {
+	request := CreateRepoRequest{
+		Name:        "golang-microservice",
+		Description: "microservice in golang",
+		Homepage:    "https://github.com",
+		Private:     true,
+		HasIssues:   true,
+		HasProjects: true,
+		HasWiki:     false,
+	}
+
+	bytes, err := json.Marshal(request)
+	assert.Nil(t, err)
+	assert.NotNil(t, bytes)
+
+	var target CreateRepoRequest
+	err = json.Unmarshal(bytes, &target)
+	assert.Nil(t, err)
+	assert.EqualValues(t, target.Name, request.Name)
+}
